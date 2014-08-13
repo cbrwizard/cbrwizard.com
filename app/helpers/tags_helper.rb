375e81604ac660d5e031b7e0e4cbdec027d65c06
cbrwizard <cbrwizard@gmail.com>
2014-08-13 19:40:36 +0400

    Sidebar categories

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -3,6 +3,7 @@ module TagsHelper
 
   # Renders tags of a certain article
   # @param record [Article]
+  # @param number [Integer] amount of tags to display
   def show_article_tags(record, number)
     record.tag_list.take(number).map do |tag|
       link_to("#" + tag, articles_path(tag: tag))
@@ -11,10 +12,23 @@ module TagsHelper
 
   # Renders tags of a certain project
   # @param record [Project]
+  # @param number [Integer] amount of tags to display
   def show_project_tags(record, number)
     record.tag_list.take(number).map do |tag|
       link_to("#" + tag, projects_path(tag: tag))
     end.join(", ").html_safe
+  end
+
+  # Renders most popular categories
+  # @param number [Integer] amount of tags to display
+  def popular_categories(number)
+    Article.popular_categories(number)
+  end
+
+  # Returns largest popularity of category
+  # @return [Integer]
+  def most_popular_category_amount
+    Article.most_popular_category_amount
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -13,4 +13,8 @@ class Article < ActiveRecord::Base
   acts_as_taggable
   acts_as_taggable_on :categories
 
+  scope :popular_categories, -> (number) {category_counts.order(:taggings_count).reverse_order.limit(number)}
+
+  scope :most_popular_category_amount, -> {category_counts.pluck(:taggings_count).max}
+
 end
