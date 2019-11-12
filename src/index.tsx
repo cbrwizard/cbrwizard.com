62a000ac1caa524fa0816cf40d4b434f80d04f47
cbrwizard <cbrwizard@gmail.com>
2019-11-12 21:33:38 +1300

    Wipes out meteor and backend

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+Sentry.init({dsn: "https://2e2d9b73143b467e8229ca638e43c73a@sentry.io/1816471"})
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
